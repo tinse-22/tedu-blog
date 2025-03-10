1e5f0fe87d8cf21a54614ce903a396051e88a674
Trung Tin <tinvtse@gmail.com>
2025-03-10 22:00:07 +0700

    Config Identity

---
+++ src/TeduBlog.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TeduBlog.Core.Domain.Identity
+{
+    [Table("AppRoles")] 
+    public class AppRole : IdentityRole<Guid> //Kiểu dữ liệu cho khóa chính
+    {
+        [Required]
+        [MaxLength(250)]
+        public string DisplayName { get; set; } // Add thêm DisplayName
+    }
+}
